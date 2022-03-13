df5156841455d30563a82ee8a917a95a4c0841df
Jiminger <jamespritchie.93@gmail.com>
2022-03-13 12:31:41 -0400

    added simple hello world for practice

--- src/main/java/com/ritchie/magic8ball/Magic8BallApplication.java
+++ src/main/java/com/ritchie/magic8ball/Magic8BallApplication.java
@@ -2,12 +2,19 @@ package com.ritchie.magic8ball;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class Magic8BallApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Magic8BallApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 
 }
